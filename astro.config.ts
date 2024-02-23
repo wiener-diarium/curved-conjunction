32a7ff321147958c0591300df21fe4da078a5a18
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-02-23 10:29:56 +0100

    adding keystatic

--- astro.config.ts
+++ astro.config.ts
@@ -1,32 +1,41 @@
+import markdoc from "@astrojs/markdoc";
+import node from "@astrojs/node";
+import react from "@astrojs/react";
+import keystatic from "@keystatic/astro";
 import { defineConfig } from "astro/config";
 import icon from "astro-icon";
-import markdoc from "@astrojs/markdoc";
-import keystatic from '@keystatic/astro';
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon({
-    include: {
-      lucide: ["*"]
-    },
-    svgoOptions: {
-      multipass: true,
-      plugins: [{
-        name: "preset-default",
-        params: {
-          overrides: {
-            removeViewBox: false
-          }
-        }
-      }]
-    }
-  }), markdoc(), keystatic()],
-  output: 'hybrid',
-  server: {
-    port: 3000
-  },
-  site: process.env.PUBLIC_APP_BASE_URL,
-  adapter: vercel()
+	integrations: [
+		icon({
+			include: {
+				lucide: ["*"],
+			},
+			svgoOptions: {
+				multipass: true,
+				plugins: [
+					{
+						name: "preset-default",
+						params: {
+							overrides: {
+								removeViewBox: false,
+							},
+						},
+					},
+				],
+			},
+		}),
+		markdoc(),
+		keystatic(),
+		react(),
+	],
+	output: "hybrid",
+	server: {
+		port: 3000,
+	},
+	site: process.env.PUBLIC_APP_BASE_URL,
+	adapter: node({
+		mode: "standalone",
+	}),
 });
