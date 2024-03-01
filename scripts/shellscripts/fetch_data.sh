e3554db720c71b78416e673f2999cba297221a74
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-03-01 14:29:05 +0100

    fixed fetch script

--- scripts/shellscripts/fetch_data.sh
+++ scripts/shellscripts/fetch_data.sh
@@ -1,11 +1,11 @@
 # bin/bash
 
 echo "fetching transkriptions from data_repo"
-rm -rf data/editions && mkdir data/editions
+rm -rf data && mkdir data
 curl -LO https://github.com/wiener-diarium/wr-data/archive/refs/heads/main.zip
 unzip main
 
-mkdir ./data/editions
+mkdir data/editions
 mkdir ./data/editions/legacy
 mkdir ./data/editions/present
 mv ./wr-data-main/data/wrd-legacy/*.xml ./data/editions/legacy
