14bd7589e2ca85d52caaf4ee16ba9b1a46e76bf5
Daniel_Stoxreiter <daniel.stoxreiter@outlook.de>
2024-05-06 15:40:11 +0200

    outsourced noske-search

--- openapi-ts.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "@hey-api/openapi-ts";
-
-export default defineConfig({
-	client: "xhr",
-	base: "https://diarium-noske.acdh-dev.oeaw.ac.at",
-	format: "prettier",
-	input: "https://raw.githubusercontent.com/acdh-oeaw/noske-ubi9/main/openapi/openapi.yaml",
-	output: "./src/client",
-});
